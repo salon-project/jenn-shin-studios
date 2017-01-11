dc5888bbbbe907469711e03bc22b86a8e0d935d3
Mike <mike.chi4@gmail.com>
2017-01-10 22:21:05 -0500

    styled home view

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 angular.module('jennShinApp', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home',{
+            url:'/',
+            templateUrl: "../views/home.html"
+        })
+      
+    $urlRouterProvider
+        .otherwise('/');
+});
